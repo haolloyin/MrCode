f3a4da7f05518cae44bc1d9752b7fe026965a597
haoio <haolloyin@163.com>
2015-08-30 13:13:06 +0800

    解析 README 接口返回的 base64 内容

--- MrCode/GitHubAPIClient/Models/GITRepository.m
+++ MrCode/GitHubAPIClient/Models/GITRepository.m
@@ -310,10 +310,8 @@ static NSString *MyOwnedRepositories = @"MrCode_MyOwnedRepositories";
     NSString *url = [NSString stringWithFormat:@"/repos/%@/readme", self.fullName];
     return [client getWithURL:url parameters:nil success:^(AFHTTPRequestOperation *operation, NSDictionary *dict) {
         NSString *base64String = dict[@"content"];
-        NSLog(@"base64String:\n%@", base64String);
-//        NSData *decodedData = [[NSData alloc] initWithBase64EncodedString:base64String options:0];
-        NSData *decodedData = [[NSData alloc] initWithBase64EncodedString:@"V2ViVmlld0phdmFzY3JpcHRCcmlkZ2UKPT09PT09PT09PT09PT09PT09PT0" options:NSDataBase64DecodingIgnoreUnknownCharacters];
-        NSLog(@"decodedData:\n%@", decodedData);
+        NSData *decodedData = [[NSData alloc] initWithBase64EncodedString:base64String options:NSDataBase64DecodingIgnoreUnknownCharacters];
+        NSLog(@"decodedData:\n%@", @(decodedData == nil));
         NSString *content = [[NSString alloc] initWithData:decodedData encoding:NSUTF8StringEncoding];
         NSLog(@"content:\n%@", content);
         success(content);
